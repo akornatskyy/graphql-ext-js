e78d0fb1f781535321d34e73466651e141ffb53b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2022-09-29 20:22:41 +0300

    feat: added misc paths.

--- src/misc/index.ts
+++ src/misc/index.ts
@@ -2,4 +2,5 @@ export {IdArgs, InputArgs} from './args';
 export {fields} from './fields';
 export {formatError} from './format';
 export {merge, Resolver, Resolvers} from './merge';
+export {paths} from './paths';
 export {validate, ValidationRule, Variables} from './validate';
